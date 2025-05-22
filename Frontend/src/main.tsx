1ae1b44919e926377340113ace09219466a28796
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2025-05-22 23:26:41 +0200

    Added Material UI

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const LIGHT_THEME_AVAILABLE = true;
+const DARK_THEME_AVAILABLE = true;
+
+const theme = createTheme({
+  colorSchemes: {
+    light: LIGHT_THEME_AVAILABLE && {
+      palette: {},
+    },
+    dark: DARK_THEME_AVAILABLE && {
+      palette: {},
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
