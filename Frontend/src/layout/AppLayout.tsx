24310ed85fd9a1a63ef22d2c3658560c3edc7dee
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2025-06-08 20:09:19 +0000

    Added icon on app bar

--- Frontend/src/layout/AppLayout.tsx
+++ Frontend/src/layout/AppLayout.tsx
@@ -1,23 +1,32 @@
+import { useTheme } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, Link as RouterLink } from "react-router-dom";
 
 export default function AppLayout() {
   const navigate = useNavigate();
+  const theme = useTheme();
 
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Typography
-            variant="h3"
-            onClick={() => navigate("/")}
-            sx={{ cursor: "pointer" }}
-          >
-            Ultimate Verdict
-          </Typography>
+          <Link component={RouterLink} to={"/"} underline="none">
+            <Typography
+              variant="h4"
+              display="flex"
+              alignItems="center"
+              sx={{
+                color: theme.palette.text.primary,
+              }}
+            >
+              <img src="/ultimate_verdict.svg" style={{ height: "1em" }} />
+              Ultimate Verdict
+            </Typography>
+          </Link>
           <Button color="inherit" onClick={() => navigate("/home")}>
             Home
           </Button>
