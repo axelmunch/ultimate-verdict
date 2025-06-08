1bc4edb14a047916c59c04e03f00fba08ad7ad9e
Robin <robin.bechlem@gmail.com>
2025-06-08 22:55:49 +0200

    ▶️ features : nouveau modèles avec des contraintes sur les données, comme required et des foreigns key

--- Database/Model.cs
+++ Database/Model.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 
 namespace Database;
 
 public class DatabaseContext : DbContext
 {
-    public DbSet<Options> Options { get; set; }
+    public DbSet<Option> Options { get; set; }
+    public DbSet<Result> Results { get; set; }
+    public DbSet<Decision> Decisions { get; set; }
+    public DbSet<Vote> Votes { get; set; }
+    public DbSet<Round> Rounds { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
@@ -14,10 +16,98 @@ public class DatabaseContext : DbContext
         options.UseNpgsql(connectionString);
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Option>(entity =>
+        {
+            entity.Property(o => o.Id).IsRequired();
+            entity.ToTable("Option");
+        });
+
+        modelBuilder.Entity<Result>(entity =>
+        {
+            entity.Property(r => r.Res)
+                .IsRequired()
+                .HasMaxLength(12);
+
+            entity.Property(r => r.Id).IsRequired();
+
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Results_Res", "res IN ('winner', 'draw', 'inconclusive')");
+            });
+        });
+
+        modelBuilder.Entity<Decision>(entity =>
+        {
+            entity.Property(d => d.Id).IsRequired();
+            entity.ToTable("Decision");
+        });
+
+        modelBuilder.Entity<Vote>(entity =>
+        {
+            entity.Property(v => v.Visibility).IsRequired();
+            entity.Property(v => v.Type).IsRequired();
+            entity.Property(v => v.VictoryCondition).IsRequired();
+
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Votes_Visibility", "visibility IN ('public', 'private')");
+                t.HasCheckConstraint("CK_Votes_Type", "type IN ('plural', 'ranked', 'weighted', 'elo')");
+                t.HasCheckConstraint("CK_Votes_VictoryCondition", "victory_condition IN ('none', 'majority', 'absolute majority', '2/3 majority', 'last man standing')");
+            });
+        });
+
+        modelBuilder.Entity<Round>(entity =>
+        {
+            entity.Property(r => r.Id).IsRequired();
+        });
+    }
+}
+public class Option
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
 
-public class Options
+public class Result
 {
-    public int id { get; set; }
-    public string name { get; set; }
+    public int Id { get; set; }
+    public string Res { get; set; }
+}
+
+public class Decision
+{
+    public int Id { get; set; }
+    public int Score { get; set; }
+}
+
+public class Vote
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public bool LiveResults { get; set; } = false;
+    public string Visibility { get; set; }
+    public string Type { get; set; }
+    public int NbRounds { get; set; }
+    public string VictoryCondition { get; set; }
+    public bool ReplayOnDraw { get; set; } = false;
+
+    public int? ResultId { get; set; }
+    public Result? Result { get; set; }
+
+    public ICollection<Round> Rounds { get; set; }
+}
+
+public class Round
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+    public int VoteId { get; set; }
+    public Vote Vote { get; set; }
+
 }
