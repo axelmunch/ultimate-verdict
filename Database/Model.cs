5bcc8d978321d686e9873a56d2b4a6f1526d598e
Robin <robin.bechlem@gmail.com>
2025-06-08 12:02:28 +0200

    ▶️ features : Passage à entity framework + Migration init.

---
+++ Database/Model.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Database;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Options> Options { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        var connectionString = "Host=localhost;Port=5432;Database=db;Username=user;Password=password";
+        options.UseNpgsql(connectionString);
+    }
+
+}
+
+public class Options
+{
+    public int id { get; set; }
+    public string name { get; set; }
+}
